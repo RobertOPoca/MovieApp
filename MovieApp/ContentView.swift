b987e3917b17ab4b7df3f884aee5d639b5f6df25
Roberto Pocasangre <pocasangre870@gmail.com>
2023-06-27 10:13:12 -0600

    :tada: Init App

---
+++ MovieApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieApp
+//
+//  Created by Roberto De la O Pocasangre on 27/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Roberto De la O Pocasangre on 27/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
